2eb6f32a4d771cf22b360562790118c0ede74458
s.111chikiii <s.111chikiii@gmail.com>
2014-09-30 16:39:53 +0900

    add json comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.json { render :json => @post.to_json(:include => [:comments]) }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
