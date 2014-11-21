61f96de26c53b512f93e210b805c83e4f46a2450
s.111chikiii <s.111chikiii@gmail.com>
2014-11-22 07:10:26 +0900

    add csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticicity_token # csrf対策の無効化
   before_filter :authenticate_user!
 end
